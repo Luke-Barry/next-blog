631603bc55fd3d36341129d2a0071e95a81fa9b0
Luke Barry <lukebarry100@gmail.com>
2023-10-04 19:27:01 +0100

    add social link endpoints

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,15 @@ export default function Navbar () {
     return (
         <div className={styles.container}>
             <div className={styles.social}>
-                <Image className={styles.logo} src="/facebook.png" alt="facebook" width={32} height={32}/>
-                <Image className={styles.logo} src="/instagram.png" alt="instagram" width={32} height={32}/>
-                <Image className={styles.logo} src="/tiktok.png" alt="tiktok" width={32} height={32}/>
-                <Image className={styles.logo} src="/youtube.png" alt="youtube" width={32} height={32}/>
+                <Link href="https://www.facebook.com/" target="_blank">
+                    <Image className={styles.logo} src="/facebook.png" alt="facebook" width={32} height={32}/>
+                </Link>
+                <Link href="https://www.instagram.com/lukejbarry/" target="_blank">
+                    <Image className={styles.logo} src="/instagram.png" alt="instagram" width={32} height={32}/>
+                </Link>
+                <Link href="https://www.youtube.com/channel/UCJOQPC1_V3gsP85RszbLcrg" target="_blank">
+                    <Image className={styles.logo} src="/youtube.png" alt="youtube" width={32} height={32}/>
+                </Link>
             </div>
             <div className={styles.links}>
                 <ThemeToggle/>
